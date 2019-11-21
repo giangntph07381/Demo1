c0f77ff4ce0e50bcf9810e4fb277a45b29d2b221
GiangDev <nguyentruonggiang20112000@gmail.com>
2019-11-22 05:10:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/demo1/ui/New/category_new.java
@@ -0,0 +1,16 @@
+package com.example.demo1.ui.New;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.demo1.R;
+
+public class category_new extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_new);
+    }
+}
